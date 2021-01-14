dde203f2cbf4219860f943cba1aaeaebf5d8ab78
jefferymason <jefferymason72@gmail.com>
2021-01-14 14:08:25 -0600

    Initial Commit

---
+++ GitTestSwift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitTestSwift
+//
+//  Created by Jeff Mason on 1/14/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GitTestSwift/GitTestSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitTestSwiftApp.swift
+//  GitTestSwift
+//
+//  Created by Jeff Mason on 1/14/21.
+//
+
+import SwiftUI
+
+@main
+struct GitTestSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
